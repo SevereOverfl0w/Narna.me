8b8a8a2b316ffc44e3e64806b7562bedbd6cb80a
SevereOverfl0w <overfl0w@narna.co>
2013-12-24 18:24:31 +0000

    Handle errors with the ping

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -21,7 +21,12 @@ $(window).resize(calcBoxes);
 
 function updateCount(){ 
             $.getJSON('https://api.syfaro.net/minecraft/1.2/server/info?ip=sv.narna.me', function(data){
-                $('#playerCount').text(data.players + " / " + data.maxPlayers + " Online");
+                var el = $('#playerCount')
+                if data.status == "error"{
+                    el.text(data.error);
+                } else {
+                    el.text(data.players + " / " + data.maxPlayers + " Online");
+                }
             });
 }
 
