4d9360fb89d68bb43e77ce180f0b19751ed90ca4
Connor Peet <connor@connorpeet.com>
2014-01-01 17:56:14 -0500

    Optimized/cleaned up main.js

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,14 +1,18 @@
-function calcBoxes(){
-    var currentTallest = 0;
-    $('.option').css('height', '');
-    $('.donate').css('margin-top', ''); // Height of box is affected by this margin
+var $options = $('.option');
+var $donate = $('.donate');
 
-    $('.option').each(function(){
+var calcBoxes = function() {
+    var currentTallest = 0;
+
+    $options.css('height', '');
+    $donate.css('margin-top', ''); // Height of box is affected by this margin
+
+    $options.each(function(){
         $el = $(this);
         currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
     });
 
-    $('.option').each(function(){
+    $options.each(function(){
         $el = $(this);
         $el.children('.donate').css('margin-top', currentTallest-$el.height());
         $el.height(currentTallest);
@@ -19,20 +23,21 @@ function calcBoxes(){
 $(window).load(calcBoxes);
 $(window).resize(calcBoxes);
 
-function updateCount(){ 
-            $.getJSON('https://api.syfaro.net/minecraft/1.2/server/info?ip=sv.narna.me', function(data){
-                var el = $('#playerCount');
-                if (data.status == "error") {
-                    el.text(data.error);
-                } else {
-                    el.text(data.players + " / " + data.maxPlayers + " Online");
-                }
-            });
-}
+var updateCount;
+var $playercount = $('#playerCount');
 
-updateCount();
+(updateCount = function() { 
+    $.getJSON('https://api.syfaro.net/minecraft/1.2/server/info?ip=sv.narna.me', function(data){
+        if (data.status == "error") {
+            $playercount.text(data.error);
+        } else {
+            $playercount.text(data.players + " / " + data.maxPlayers + " Online");
+        }
+    });
+})();
+
 setInterval(updateCount, 5000);
 
 $('.alert .close').click(function(){
-    $(this).parent().fadeOut(function(){ $(this).remove()});
+    $(this).parent().fadeOut($(this).remove);
 });
